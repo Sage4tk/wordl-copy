7e062af8db8db40685201ee307a0ea58a36b74c6
timothy timbol <ttimbol08@gmail.com>
2022-06-22 23:09:05 +0400

    changed algorithm for answer

--- src/components/Row.js
+++ src/components/Row.js
@@ -3,39 +3,31 @@ import { useEffect, useState } from "react"
 export default function Row({ answer, hidden, curRow, index }) {
 
     //check if already submitted
-    const [submit, setSubmit] = useState(false);
+    const [submit, setSubmit] = useState([]);
 
     useEffect(() => {
         if (curRow > index) {
-            setSubmit(true);
+            let sHidden = [...hidden.split("")];
+
+            setSubmit(answer.split("").map((e, index) => {
+                if (e === sHidden[index]) {
+                    sHidden[index] = 0;
+                    console.log(sHidden)
+                    return "green";
+                }
+                
+                return "grey"
+            }))
         }
     }, [curRow]);
 
-    const colorCells = (bool, letter1, letter2) => {
-        if (bool) {
-            if (letter1 === letter2) {
-                return {
-                    background: "green"
-                }
-            } else if (hidden.includes(letter1)) {
-                return {
-                    background: "yellow"
-                }
-            } else {
-                return {
-                    background: "grey"
-                }
-            }
-        }
-    }
-
     return (
-        <div className="row">
-            <div className="column" style={colorCells(submit, answer[0], hidden[0])} >{answer[0] ? answer[0]:""}</div>
-            <div className="column" style={colorCells(submit, answer[1], hidden[1])} >{answer[1] ? answer[1]:""}</div>
-            <div className="column" style={colorCells(submit, answer[2], hidden[2])} >{answer[2] ? answer[2]:""}</div>
-            <div className="column" style={colorCells(submit, answer[3], hidden[3])} >{answer[3] ? answer[3]:""}</div>
-            <div className="column" style={colorCells(submit, answer[4], hidden[4])} >{answer[4] ? answer[4]:""}</div>
+        <div className="row" onClick={() => {console.log(submit)}}>
+            <div className="column">{answer[0] ? answer[0]:""}</div>
+            <div className="column">{answer[1] ? answer[1]:""}</div>
+            <div className="column">{answer[2] ? answer[2]:""}</div>
+            <div className="column">{answer[3] ? answer[3]:""}</div>
+            <div className="column">{answer[4] ? answer[4]:""}</div>
         </div>
     )
 }
